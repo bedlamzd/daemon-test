d99c6eb776e989ec1f9ca71bf8c51fcb7a066500
bedlamzd <bedlamzd@gmail.com>
2020-07-16 12:53:04 +0300

    http side of daemon   get requests to download   post requests to upload   function to delete file

---
+++ demon.py
@@ -0,0 +1,83 @@
+import urllib
+import hashlib
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
+from pathlib import Path
+
+STORAGE = 'storage'
+HOST = 'localhost'
+PORT = 8080
+
+
+def return_file(file_hash):
+    try:
+        with open(f'{STORAGE}/{file_hash[:2]}/{file_hash}', 'rb') as f:
+            file = f.read()
+        return file
+    except:
+        print('No File')
+
+
+def store_file(file):
+    file_hash = hashlib.md5(file).hexdigest()
+    path = Path(f'{STORAGE}/{file_hash[:2]}/{file_hash}')
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with open(path, 'wb') as f:
+        f.write(file)
+    print(f'File saved. Hash: {file_hash}')
+    return file_hash
+
+
+def delete_file(file_hash):
+    if (file := Path(f'{STORAGE}/{file_hash[:2]}/{file_hash}')).exists():
+        file.unlink()
+        print('File deleted.')
+        try:
+            file.parent.rmdir()
+            print('Removed empty dir.')
+        except OSError:
+            pass
+        return True
+    return False
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print('get request')
+        self.send_response(200)
+        query = urllib.parse.urlparse(self.path).query
+        params = urllib.parse.parse_qs(query)
+        if (file_hash := params.get('file_hash', False)) and (file := return_file(*file_hash)) is not None:
+            self.send_header('Content-type', 'file')
+            self.end_headers()
+            self.wfile.write(file)
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        file_hash = store_file(body)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'Hash: ')
+        response.write(file_hash.encode('utf-8'))
+        self.wfile.write(response.getvalue())
+
+    def do_DELETE(self):
+        # TODO
+        pass
+
+
+if __name__ == '__main__':
+    # TODO: make real daemon out of it
+    web_server = HTTPServer((HOST, PORT), MyServer)
+    print('Server running')
+
+    try:
+        web_server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    web_server.server_close()
+    print("Server stopped.")
